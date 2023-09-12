ca15426a2c15f6b7b59fd9ca616454b58db2f95f
naveena-devaraj16 <123715928+naveena-devaraj16@users.noreply.github.com>
2023-09-12 09:35:26 +0530

    changed

---
+++ main.js
@@ -0,0 +1,10 @@
+function updateDateTime() {
+    const currentDate = new Date();
+    const dateElement = document.getElementById("date");
+    const timeElement = document.getElementById("time");
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    dateElement.textContent = currentDate.toLocaleDateString(undefined, options);
+    timeElement.textContent = currentDate.toLocaleTimeString();
+    }
+    updateDateTime();
+    setInterval(updateDateTime, 1000);
