9ab0de47a37dc4ed1179c0d93b7e6d4b665f5088
Keerthana-2005 <144401311+Keerthana-2005@users.noreply.github.com>
2023-09-15 10:20:46 +0530

    node completed

--- Node.js
+++ Node.js
@@ -1,1 +1,25 @@
 const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'ev_charging',
+});
+
+connection.connect((err) => {
+    if (err){
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+
+
+    connection.end((err) => {
+        if(err){
+            console.error('Error closing MySQL connection: ', err);
+            return;
+        }
+        console.log('MySQL connection closed');
+    });
+});
