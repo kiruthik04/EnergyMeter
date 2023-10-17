271bccca66d4e7372d3d1e5aa825f4c9046663c5
Kiruthik Varshan K R <kiruthikvarshan.al22@bitsathy.ac.in>
2023-10-17 10:46:23 +0530

    Changes made

--- main.js
+++ main.js
@@ -9,13 +9,3 @@ function updateDateTime() {
     updateDateTime();
     setInterval(updateDateTime, 1000);
 
-    var firebaseConfig = {
-        apiKey: "AIzaSyAoWfqArgFdGSkcK0sH-SIe2PRr7ciTpWc",
-        authDomain: "ev-charging-station-caada.firebaseapp.com",
-        projectId: "ev-charging-station-caada",
-        storageBucket: "ev-charging-station-caada.appspot.com",
-        messagingSenderId: "72126582235",
-        appId: "1:72126582235:web:fd55b758ac4e1d80456bfe"
-      };
-      firebase.initializeApp(firebaseConfig);
-    //elements added
